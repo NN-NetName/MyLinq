1a6309b6149f52daa1abeb522eca81d17cbd4900
NN-NetName <nn.netname@gmail.com>
2023-11-17 19:47:38 +0600

    Initial commit

---
+++ MuLinq/IntArrayExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace MuLinq
+{
+    public static class IntArrayExtensions
+    {
+        public static TSource[] Where<TSource>(this TSource[] source, Predicate<TSource> criterion)
+        {
+
+            if (source == null)
+                throw new InvalidOperationException();
+
+            var result = new List<TSource>();
+            foreach (var item in source)
+            {
+                if (criterion?.Invoke(item) == true)
+                    result.Add(item);
+            }
+
+            return result.ToArray();    
+        }
+    }
+}
